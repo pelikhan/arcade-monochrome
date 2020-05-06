13e0a64bd5e9e8e340714751aa042f0e9b40263f
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-05-05 23:14:24 -0700

    updated sample

--- test.ts
+++ test.ts
@@ -1,6 +1,7 @@
 game.onPaint(function () {
     for(let i = 0; i < 16; ++i) {
-        screen.fillRect((i % 4) * 20, Math.floor(i / 4) * 20, 16, 16, i);
+        screen.fillRect(14 + (i % 4) * 20, 14 + Math.floor(i / 4) * 20, 20, 20, 1);
+        screen.fillRect(16 + (i % 4) * 20, 16 + Math.floor(i / 4) * 20, 16, 16, i);
     }
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
